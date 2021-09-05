7ffc6927b8ffe56d13aa8c70a9d8b140ee25d03d
Alex de Ruijter <48787588+AlexdeRuijter@users.noreply.github.com>
2021-09-05 15:09:56 +0200

    Started implementing a forwardEuler numerical scheme

--- schemes/schemes.go
+++ schemes/schemes.go
@@ -2,10 +2,36 @@ package schemes
 
 import "github.com/AlexdeRuijter/stochastic-particle-model/brownian"
 
-type NumericSchemeer interface {
+type NumericScheme interface {
 	Update(float64) float64
 }
 
-type ForwardEuler struct {
-	W brownian.BrownianState
+type ForwardEuler interface {
+	Update(float64) float64
+	NewForwardEuler(int64,
+		[2]float64,
+		func([2]float64) float64,
+		func([2]float64) float64,
+	) forwardEuler
+	GetPosition() [2]float64
+	GetRandomState() brownian.BrownianState
+}
+type forwardEuler struct {
+	w        brownian.BrownianState
+	position [2]float64
+	f        func([2]float64) float64
+	g        func([2]float64) float64
+}
+
+func NewForwardEuler(seed int64,
+	position [2]float64,
+	f func([2]float64) float64,
+	g func([2]float64) float64) *forwardEuler {
+	b := brownian.NewBrownianState(seed)
+	return &forwardEuler{
+		w:        b,
+		position: position,
+		f:        f,
+		g:        g,
+	}
 }
