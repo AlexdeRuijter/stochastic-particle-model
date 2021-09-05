08a018b7f435e7ddfd137d4c816318f96591c070
Alex de Ruijter <48787588+AlexdeRuijter@users.noreply.github.com>
2021-09-05 17:25:30 +0200

    Just some improvements over the original one

--- main.go
+++ main.go
@@ -4,12 +4,22 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/AlexdeRuijter/stochastic-particle-model/brownian"
+	"github.com/AlexdeRuijter/stochastic-particle-model/schemes"
 )
 
+func f(position [2]float64) [2]float64 {
+	return position
+}
+
+func g(position [2]float64) [2]float64 {
+	return position
+}
+
 func main() {
-	b := brownian.NewBrownianState(time.Now().UnixNano())
-	dt := 0.01
-	dW := b.Timestep(dt)
-	fmt.Println("Increment is: ", dW, "\nFor timestep: ", dt)
+	var initial_position [2]float64
+	initial_position = [2]float64{.5, .5}
+	scheme := schemes.NewForwardEuler2D(time.Now().UnixNano(), initial_position, f, g)
+	scheme.Update(0.001)
+	initial_position = scheme.GetPosition()
+	fmt.Println(initial_position)
 }
